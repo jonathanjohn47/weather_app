13d0c145fee3556200118e719fc3c147d0b6f899
jonathanjohn <jonathanjohn1133@gmail.com>
2023-12-28 11:09:01 +0530

    Error handling

--- lib/home/get_controllers/home_get_controller.dart
+++ lib/home/get_controllers/home_get_controller.dart
@@ -22,87 +22,107 @@ class HomeGetController extends GetxController {
   TextEditingController searchController = TextEditingController();
 
   Future<Position> determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+    try {
+      bool serviceEnabled;
+      LocationPermission permission;
 
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
+      // Test if location services are enabled.
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        // Location services are not enabled don't continue
+        // accessing the position and request users of the
+        // App to enable the location services.
+        return Future.error('Location services are disabled.');
       }
-    }
 
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // Permissions are denied, next time you could try
+          // requesting permissions again (this is also where
+          // Android's shouldShowRequestPermissionRationale
+          // returned true. According to Android guidelines
+          // your App should show an explanatory UI now.
+          return Future.error('Location permissions are denied');
+        }
+      }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      // When we reach here, permissions are granted and we can
+      // continue accessing the position of the device.
+      return await Geolocator.getCurrentPosition();
+    } catch (e) {
+      Get.snackbar("Error", e.toString(),
+          backgroundColor: Colors.red, colorText: Colors.white);
+      throw e;
+    }
   }
 
   Future<WeatherResponseModel> loadWeatherData() async {
-    isLoading.value = true;
-    Position position = await determinePosition();
+    try {
+      isLoading.value = true;
+      Position position = await determinePosition();
 
-    print("position: ${position.latitude}, ${position.longitude}");
-    var headers = {'accept': 'application/json'};
-    var request = http.Request(
-        'GET',
-        Uri.parse(
-            'https://api.weatherapi.com/v1/current.json?q=${position.latitude}%2C${position.longitude}&lang=en&key=5b276e6121574a8fb2f192325232712'));
+      print("position: ${position.latitude}, ${position.longitude}");
+      var headers = {'accept': 'application/json'};
+      var request = http.Request(
+          'GET',
+          Uri.parse(
+              'https://api.weatherapi.com/v1/current.json?q=${position.latitude}%2C${position.longitude}&lang=en&key=5b276e6121574a8fb2f192325232712'));
 
-    request.headers.addAll(headers);
+      request.headers.addAll(headers);
 
-    http.StreamedResponse response = await request.send();
+      http.StreamedResponse response = await request.send();
 
-    if (response.statusCode == 200) {
-      String responseString = await response.stream.bytesToString();
-      weatherResponseModel.value = weatherResponseModelFromJson(responseString);
-      isLoading.value = false;
-      return weatherResponseModelFromJson(responseString);
-    } else {
-      throw Exception('Failed to load weather data');
+      if (response.statusCode == 200) {
+        String responseString = await response.stream.bytesToString();
+        weatherResponseModel.value =
+            weatherResponseModelFromJson(responseString);
+        isLoading.value = false;
+        return weatherResponseModelFromJson(responseString);
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      Get.snackbar("Error", e.toString(),
+          backgroundColor: Colors.red, colorText: Colors.white);
+      throw e;
     }
   }
 
   Future<WeatherResponseModel> loadWeatherDataForLocation(
       double lat, double long) async {
-    isLoading.value = true;
-    var headers = {'accept': 'application/json'};
-    var request = http.Request(
-        'GET',
-        Uri.parse(
-            'https://api.weatherapi.com/v1/current.json?q=${lat}%2C${long}&lang=en&key=5b276e6121574a8fb2f192325232712'));
+    try {
+      isLoading.value = true;
+      var headers = {'accept': 'application/json'};
+      var request = http.Request(
+          'GET',
+          Uri.parse(
+              'https://api.weatherapi.com/v1/current.json?q=${lat}%2C${long}&lang=en&key=5b276e6121574a8fb2f192325232712'));
 
-    request.headers.addAll(headers);
+      request.headers.addAll(headers);
 
-    http.StreamedResponse response = await request.send();
+      http.StreamedResponse response = await request.send();
 
-    if (response.statusCode == 200) {
-      String responseString = await response.stream.bytesToString();
-      weatherResponseModel.value = weatherResponseModelFromJson(responseString);
-      isLoading.value = false;
-      return weatherResponseModel.value;
-    } else {
-      throw Exception('Failed to load weather data');
+      if (response.statusCode == 200) {
+        String responseString = await response.stream.bytesToString();
+        weatherResponseModel.value =
+            weatherResponseModelFromJson(responseString);
+        isLoading.value = false;
+        return weatherResponseModel.value;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      Get.snackbar("Error", e.toString(),
+          backgroundColor: Colors.red, colorText: Colors.white);
+      throw e;
     }
   }
 
